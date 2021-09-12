ac5d28c8c5417cc1811f59005c71f74cda1def39
Guangwen <zgwmike@hotmail.com>
2021-09-12 15:17:46 -0700

    changes

--- src/main/scala/example/LongestCommonSubstring.scala
+++ src/main/scala/example/LongestCommonSubstring.scala
@@ -5,14 +5,38 @@ package example
   */
 trait LongestCommonSubstring {
   object Solution1 {
-    def solve(in: String): Int = in
-      .foldLeft(Map.empty[String, Int]) { case (s, c) =>
-        ???
-      }
-      .size
+    def solve(in: String): Int = {
+      def loop(
+          data: List[Char],
+          current: List[Char],
+          solution: List[Char]
+      ): List[Char] =
+        data match {
+          case Nil => if (current.size > solution.size) current else solution
+          case head :: tail =>
+            if (current.contains(head)) {
+              val newCurrent =
+                current.splitAt(current.lastIndexOf(head))._2.tail ::: List(
+                  head
+                )
+              val newSolution =
+                if (current.size >= solution.size) current
+                else solution
+              loop(tail, newCurrent, newSolution)
+            } else
+              loop(tail, current ::: List(head), solution)
+        }
+
+      loop(in.toList, Nil, Nil).size
+    }
   }
   def run() = {
     val input = "abcabcbb"
-    println(Solution1.solve(input))
+    println(Solution1.solve(" ") == 1)
+    println(Solution1.solve("abcabcbb") == 3)
+    println(Solution1.solve("bbbbbb") == 1)
+    println(Solution1.solve("pwwkew") == 3)
+
+    println(Solution1.solve("aabaab!bb") == 3)
   }
 }
